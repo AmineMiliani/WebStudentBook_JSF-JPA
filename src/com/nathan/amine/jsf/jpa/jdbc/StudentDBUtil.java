d02c3aae774f5baa25300c40b0c3d919c6bfc4eb
AmineMiliani <44442971+AmineMiliani@users.noreply.github.com>
2018-11-25 16:32:42 +0100

    Added function (edit, add and delete)

--- src/com/nathan/amine/jsf/jpa/jdbc/StudentDBUtil.java
+++ src/com/nathan/amine/jsf/jpa/jdbc/StudentDBUtil.java
@@ -15,7 +15,8 @@ import javax.sql.DataSource;
 public class StudentDBUtil {
 	private DataSource dataSource;
 
-	public StudentDBUtil(DataSource theDataSource) {
+
+	public StudentDBUtil() {
 		try {
 			dataSource = getDataSource();
 		} catch (NamingException e) {
@@ -24,11 +25,12 @@ public class StudentDBUtil {
 		}
 	}
 
+
 	public DataSource getDataSource() throws NamingException{
 		String jndi="java:comp/env/jdbc/studentdb";
 		Context context = new InitialContext();DataSource dataSource = (DataSource) context.lookup(jndi);
 		return dataSource;
-		}
+	}
 
 	public List<Student> getStudents() throws Exception {
 		List<Student> students= new ArrayList<Student>();
@@ -38,7 +40,7 @@ public class StudentDBUtil {
 		try {
 			myConn = dataSource.getConnection();
 			myStmt= myConn.createStatement();
-			String sql= "select * from student order by last_name";
+			String sql= "select * from student order by id";
 			myRs = myStmt.executeQuery(sql);
 			while(myRs.next()){
 				int id = myRs.getInt("id");
@@ -88,5 +90,65 @@ public class StudentDBUtil {
 			close(myConn,myStmt,myRs);
 		}
 	}
+	public Student fetchStudent(int id) {
+		Connection myConn=null;
+		Statement myStmt = null;
+		ResultSet myRs= null;
+		Student student=null;
+		try {
+			myConn = dataSource.getConnection();
+			myStmt= myConn.createStatement();
+			String sql= "select * from student where id="+id;
+			myRs = myStmt.executeQuery(sql);
+			while(myRs.next()){String firstName=myRs.getString("first_name");
+			String lastName=myRs.getString("last_name");String email = myRs.getString("email");
+			student = new Student(id,firstName,lastName,email);
+			}
+			return student;
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+			return null;
+		} 
+		finally{
+			close(myConn,myStmt,myRs);
+		}
+	}
+	
+	public void updateStudent(Student student) {
+		Connection myConn=null;
+		PreparedStatement myStmt = null;
+		try {
+		myConn = dataSource.getConnection();
+		String sql = "update student set first_name=?, last_name=?,email=? where id=?";
+		myStmt = myConn.prepareStatement(sql);
+		myStmt.setString(1, student.getFirst_name());
+		myStmt.setString(2, student.getLast_name());
+		myStmt.setString(3, student.getEmail());
+		myStmt.setInt(4,student.getId());
+		myStmt.execute();
+		}
+		catch(Exception e){
+		System.out.println(e.getMessage());
+		}
+		finally{
+		close(myConn,myStmt,null);
+		}
+		}
 
+	
+	public void deleteStudent(int id) {
+		// TODO Auto-generated method stub
+		Connection myConn=null;
+		Statement myStmt = null;
+		try {
+		myConn = dataSource.getConnection();
+		myStmt= myConn.createStatement();
+		String sql= "delete from student where id="+id;
+		myStmt.execute(sql);
+		}catch(Exception e){
+		System.out.println(e.getMessage());
+		} finally{ close(myConn,myStmt,null); }}
 }
+
---
+++ src/com/nathan/amine/jsf/jpa/jdbc/StudentManager.java
@@ -0,0 +1,101 @@
+package com.nathan.amine.jsf.jpa.jdbc;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.sql.DataSource;
+
+
+@ManagedBean (name="StudentManager")
+@SessionScoped
+public class StudentManager {
+	
+	private Student student;
+	private List<Student> students;
+	private StudentDBUtil db;	
+	private Student editStudent;
+	public StudentManager() {
+		super();
+		db = new StudentDBUtil();
+		student = new Student();
+	}
+
+
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+	
+	public List<Student> LoadStudents() {
+		try {
+			students = db.getStudents();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return students;
+	}
+	
+	public String AddStudent() {
+		db.addStudent(student);
+		student = new Student();
+		return "list-students";
+		
+	}
+	
+	public String LoadStudents(int id) {
+		editStudent = db.fetchStudent(id);
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		Map<String, Object> requestMap = externalContext.getRequestMap();
+		requestMap.put("student", editStudent);
+		return"edit-student";
+		
+	}
+	
+	public String UpdateStudent() {
+		db.updateStudent(editStudent);
+		editStudent = new Student();
+		return"list-students";
+	}
+	
+	public String DeleteStudent(int id) {
+		db.deleteStudent(id);
+		return "list-students";
+		
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+
+
+	public Student getEditStudent() {
+		return editStudent;
+	}
+
+
+
+	public void setEditStudent(Student editStudent) {
+		this.editStudent = editStudent;
+	}
+
+
+
+}
